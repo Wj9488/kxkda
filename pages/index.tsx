52d1d7821382fd35b7ee323ce1146057bf8812b9
Will Jones <wtjones10@gmail.com>
2023-06-17 22:11:51 +0100

    icon / dvh

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,22 @@ export default function Home() {
     <>
     <Head>
       <title>KXKDA</title>
+      <link rel="shortcut icon" href="/kxkda__icon.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="KXKDA - KXKDA - KXKDA - KXKDA - KXKDA"
+        />
+        <meta
+          name="description"
+          content="KXKDA - KXKDA - KXKDA - KXKDA - KXKDA"
+        />
     </Head>
-    <main className="min-h-screen flex items-center justify-center">
-      <p className="dark:text-[#fafafa] text-2xl header__letter_spacing">KXKDA</p>
+    <main className="lg:min-h-screen min-h-[100dvh] flex items-center justify-center">
+      <p className="dark:text-[#fafafa] text-3xl header__letter_spacing">KXKDA</p>
     </main>
     </>
   )
